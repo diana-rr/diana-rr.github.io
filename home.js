2b4a801646954bc759c5d488e93e56c8e8fcc6cf
unknown <diana.ramos.2013.2014@gmail.com>
2019-07-10 09:36:50 -0700

    This is my first commit!

---
+++ home.js
@@ -0,0 +1,20 @@
+function alert_name(){
+  //Create name variable
+  var name = "Diana";
+  alert("My name is " + name);
+  console.log("Test 123");
+}
+
+function change_image(){
+  var image = document.getElementById('ow_image');
+  image.src = "ow2.jpg";
+}
+function bigImg(x) {
+  x.style.height = "500px";
+  x.style.width = "500px";
+}
+
+function normalImg(x) {
+  x.style.height = "300px";
+  x.style.width = "300px";
+}
